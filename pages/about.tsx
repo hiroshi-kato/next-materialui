23f1118f11118e4de35b5b8439cb8653e3062f93
hiroshi-kato <hiroshikato1208@gmail.com>
2020-12-05 10:37:33 +0900

    Material UIを使用してindexとaboutページを書き換え

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,26 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+import Layout from '../components/Layout';
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
+  <Layout title='About | Next.js + TypeScript Example'>
+    <Container maxWidth='sm'>
+      <Box my={4}>
+        <Typography variant='h4' component='h1' gutterBottom>
+          Next.js example
+        </Typography>
+        <Link href='/' passHref>
+          <Button variant='contained' color='primary'>
+            Go to the main page
+          </Button>
+        </Link>
+      </Box>
+    </Container>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import Layout from '../components/Layout';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout title='Home | Next.js + TypeScript Example'>
+    <Container maxWidth='sm'>
+      <Box my={4}>
+        <Typography variant='h4' component='h1' gutterBottom>
+          Next.js example
+        </Typography>
+        <Link href='/about'>Go to the about page</Link>
+      </Box>
+    </Container>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
